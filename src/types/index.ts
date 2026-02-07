4882abdd3ac44a27c07ca1e323be9de7a1595be5
Matheus Costa <contato.matheusccp5@gmail.com>
2026-02-07 01:54:21 -0300

    feat: create intefaces do projeto

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface Product {
+  id: string; 
+  name: string; 
+  category: string; 
+  sizes: string[]; 
+  price: number; 
+  description?: string; 
+  imageUrl?: string; 
+  createdAt: Date; 
+  updatedAt: Date; 
+}
+
+export interface ProductFormData {
+  name: string;
+  category: string;
+  sizes: string[];
+  price: string;
+  description: string;
+  imageFile?: File; // Arquivo de imagem para upload
+}
+
+export interface User {
+  uid: string; // ID único do usuário
+  email: string; // Email do usuário
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface SelectOption {
+  value: string;
+  label: string;
+}
+
+export interface ProductFilters {
+  category: string; // "all" ou nome da categoria
+  searchTerm: string; // Termo de busca
+}
