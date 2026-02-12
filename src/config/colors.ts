a2d08190b3eb258f503d145bf2cbe8d98b962045
Matheus Costa <contato.matheusccp5@gmail.com>
2026-02-12 20:01:54 -0300

    feat: update colors

--- src/config/colors.ts
+++ src/config/colors.ts
@@ -5,6 +5,9 @@ export const COLORS: SelectOption[] = [
   { value: 'Branco', label: 'Branco' },
   { value: 'Vermelho', label: 'Vermelho' },
   { value: 'Azul', label: 'Azul' },
+  { value: 'Azul BB', label: 'Azul BB' },
+  { value: 'Azul Marinho', label: 'Azul Marinho' },
+  { value: 'Azul Cobalto', label: 'Azul Cobalto' },
   { value: 'Rosa', label: 'Rosa' },
   { value: 'Verde', label: 'Verde' },
   { value: 'Amarelo', label: 'Amarelo' },
@@ -13,5 +16,9 @@ export const COLORS: SelectOption[] = [
   { value: 'Cinza', label: 'Cinza' },
   { value: 'Bege', label: 'Bege' },
   { value: 'Marrom', label: 'Marrom' },
+  { value: 'Onça', label: 'Onça' },
+  { value: 'Terracota', label: 'Terracota' },
+  { value: 'Marsala', label: 'Marsala' },
+  { value: 'Bordô', label: 'Bordô' },
   // Adicione mais conforme necessário
 ];
