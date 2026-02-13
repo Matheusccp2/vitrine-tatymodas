e24e415cd3f7551c5d77365ddc3dbc1fa89c67d0
Matheus Costa <contato.matheusccp5@gmail.com>
2026-02-13 20:08:47 -0300

    feat: update colors and categories

--- src/config/categories.ts
+++ src/config/categories.ts
@@ -3,15 +3,18 @@ import { SelectOption } from "@/types";
 export const CATEGORIES: SelectOption[] = [
   { value: "Blusa", label: "Blusa" },
   { value: "Body", label: "Body" },
+  { value: "Bermuda", label: "Bermuda" },
   { value: "Calca", label: "Calça" },
   { value: "Casaco", label: "Casaco" },
+  { value: "Chemise", label: "Chemise" },
   { value: "Conjunto", label: "Conjunto" },
   { value: "Jaqueta", label: "Jaqueta" },
   { value: "Macaquinho", label: "Macaquinho" },
+  { value: "Pantalona", label: "Pantalona" },
+  { value: "Regata", label: "Regata" },
   { value: "Saia", label: "Saia" },
   { value: "Short", label: "Short" },
   { value: "Vestido", label: "Vestido" },
-  { value: "Chemise", label: "Chemise" },
 ];
 
 export const ALL_CATEGORIES_OPTION: SelectOption = {
